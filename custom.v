0e8ed1cdc11ecd4418bf0de87fc404bc5746c202
Leo Moser <leomoser99@gmail.com>
2026-05-01 12:07:50 +0200

    chore: update models pack
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- custom.v
+++ custom.v
@@ -1,10 +1,10 @@
-module clk_buf(input A, output X);
-assign X = A;
-endmodule
+//module clk_buf(input A, output X);
+//assign X = A;
+//endmodule
 
 // (* blackbox *)
 // blacbox prevents logic loops, but OpenROAD
 // doesn't find the instance later on...
-module break_comb_loop(input A, output X);
-assign X = A;
-endmodule
+//module break_comb_loop(input A, output X);
+//assign X = A;
+//endmodule
