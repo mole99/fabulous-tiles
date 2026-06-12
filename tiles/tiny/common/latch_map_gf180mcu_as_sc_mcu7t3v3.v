779295f23ad36f20d5315c4d56e147247986642f
Leo Moser <leomoser99@gmail.com>
2026-06-12 10:34:12 +0200

    feat: fix latch mapping for gf180mcu_as_sc_mcu7t3v3
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- tiles/tiny/common/latch_map_gf180mcu_as_sc_mcu7t3v3.v
+++ tiles/tiny/common/latch_map_gf180mcu_as_sc_mcu7t3v3.v
@@ -1,6 +1,6 @@
 // positive D-latch
 module \$_DLATCH_P_ (input E, input D, output Q);
-    gf180mcu_as_sc_mcu7t3v3__dlxfp_2 _TECHMAP_REPLACE_ (
+    gf180mcu_as_sc_mcu7t3v3__dlxtp_2 _TECHMAP_REPLACE_ (
         .ENA (E),
         .D  (D),
         .Q  (Q)
@@ -9,7 +9,7 @@ endmodule
 
 // negative D-latch
 module \$_DLATCH_N_ (input E, input D, output Q);
-    gf180mcu_as_sc_mcu7t3v3__dlxfn_2 _TECHMAP_REPLACE_ (
+    gf180mcu_as_sc_mcu7t3v3__dlxtn_2 _TECHMAP_REPLACE_ (
         .ENA (E),
         .D  (D),
         .Q  (Q)
